09cc7646b7bfc524f8001bc7f419352f520a7024
Ondrej Sika <ondrej@ondrejsika.com>
2023-05-17 10:08:14 +0200

    feat: Add logging using zerolog

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	hostname, _ := os.Hostname()
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Hello EurOpen! %s</h1>\n", hostname)
-	fmt.Printf("%s %s\n", r.Method, r.URL.Path)
-}
-
 func main() {
-	http.HandleFunc("/", index)
-	fmt.Println("Listening on 0.0.0.0:8000, http://127.0.0.1:8000")
+	HOSTNAME, _ := os.Hostname()
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "<h1>Hello EurOpen! %s</h1>\n", HOSTNAME)
+		log.Info().
+			Str("hostname", HOSTNAME).
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Msg(fmt.Sprintf("%s %s", r.Method, r.URL.Path))
+	})
+
+	log.Info().
+		Str("hostname", HOSTNAME).
+		Msg("Listening on 0.0.0.0:8000, http://127.0.0.1:8000")
 	http.ListenAndServe(":8000", nil)
 }
